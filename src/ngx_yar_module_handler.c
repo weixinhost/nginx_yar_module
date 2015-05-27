de4b6793529c56249e9745fc3ef6c667efcda982
miskolee <miskolee@pencilinside.com>
2015-05-28 00:01:23 +0800

    refix:send big data

--- src/ngx_yar_module_handler.c
+++ src/ngx_yar_module_handler.c
@@ -213,7 +213,7 @@ static char* ngx_http_yar_conf_on(ngx_conf_t *cf, ngx_command_t *cmd,void *conf)
     return NULL;
 }
 
-
+/*
 ngx_str_t *reply_data = NULL;
 
 void set_reply(ngx_str_t *reply){
@@ -226,6 +226,7 @@ ngx_str_t *get_reply(){
     return reply_data;
 
 }
+ */
 
 static char* ngx_http_yar_conf_debug(ngx_conf_t *cf, ngx_command_t *cmd,void *conf){
 
@@ -312,7 +313,7 @@ void ngx_http_yar_handler(ngx_http_request_t *r){
     memcpy(response->payload.data + sizeof(yar_header), YAR_PACKAGER, sizeof(YAR_PACKAGER));
 
 
-
+    /*
     ngx_str_t *reply = ngx_pcalloc (r->pool, sizeof (ngx_str_t));
     u_char *data  = ngx_pcalloc (r->pool, sizeof (u_char) * response->payload.size);
     memcpy(data,response->payload.data,response->payload.size);
@@ -321,15 +322,15 @@ void ngx_http_yar_handler(ngx_http_request_t *r){
     reply->data = data;
     reply->len = response->payload.size;
     set_reply(reply);
+    */
 
-    /*
+
     ngx_str_t reply;
     reply.data = (u_char *)response->payload.data;
 
     reply.len = response->payload.size;
 
     ngx_http_yar_send_response(r,&reply);
-    */
 
     goto clean_resource;
 
@@ -367,7 +368,7 @@ void ngx_http_yar_handler(ngx_http_request_t *r){
 
 ngx_int_t ngx_http_yar_read_request_handler(ngx_http_request_t *r){
 
-    set_reply(NULL);
+   // set_reply(NULL);
 
     r->request_body_in_single_buf = 1;
 
@@ -381,6 +382,7 @@ ngx_int_t ngx_http_yar_read_request_handler(ngx_http_request_t *r){
         return rc;
     }
 
+    /*
     ngx_str_t *reply = get_reply ();
 
     if(reply){
@@ -388,6 +390,7 @@ ngx_int_t ngx_http_yar_read_request_handler(ngx_http_request_t *r){
         ngx_http_yar_send_response(r,reply);
 
     }
+     */
 
-    return NGX_DONE;
+    return NGX_OK;
 }
