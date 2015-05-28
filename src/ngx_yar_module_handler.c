6690a6ab6f56dad374d56210c145b347da810480
miskolee <miskolee@pencilinside.com>
2015-05-28 22:21:11 +0800

    add run time with log

--- src/ngx_yar_module_handler.c
+++ src/ngx_yar_module_handler.c
@@ -317,7 +317,7 @@ void ngx_http_yar_handler(ngx_http_request_t *r){
     ngx_str_t *reply = ngx_pcalloc (r->pool, sizeof (ngx_str_t));
     u_char *data  = ngx_pcalloc (r->pool, sizeof (u_char) * response->payload.size);
     memcpy(data,response->payload.data,response->payload.size);
-    ngx_http_yar_send_response(r,&reply);
+    ngx_http_yar_send_response(r,reply);
 
     /*
     reply->data = data;
